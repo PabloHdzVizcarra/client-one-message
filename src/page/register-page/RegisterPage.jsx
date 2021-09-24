c4be6e2124349e947caa49d11dab69d9d454f20e
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-09-24 10:55:45 -0500

    feat create RegisterPage component

---
+++ src/page/register-page/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RegisterTemplate from "./RegisterTemplate";
+
+const RegisterPage = () => {
+  return (
+    <RegisterTemplate/>
+  );
+};
+
+export default RegisterPage;
